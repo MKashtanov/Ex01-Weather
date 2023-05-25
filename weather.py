26a6826f2430adfe834967967c8540d0947d7ee9
m.kashtanov <m.kashtanov@mkdg.ru>
2023-05-25 12:13:32 +0400

    Initial commit

---
+++ weather.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_weather(plase):
+    url = f'https://wttr.in/{plase}'
+    payload = {'nTqM': '', 'lang': 'ru'}
+    try:
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        return response.text
+    except:
+        return f'Не удалось получить погоду в {plase}'
+
+
+if __name__ == "__main__":
+    plases = ['Лондон', 'Шереметьево', 'Череповец']
+    for plase in plases:
+        print(get_weather(plase))
